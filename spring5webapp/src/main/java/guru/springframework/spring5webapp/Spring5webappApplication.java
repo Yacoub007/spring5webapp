533c76ecd60359fd55ecdb3834adfaac5836b245
Ethmane <ethmaneyac@gmail.com>
2019-08-18 15:34:57 +0200

    adding JPA example

---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		run(Spring5webappApplication.class, args);
+	}
+
+}
